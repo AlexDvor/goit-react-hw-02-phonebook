a695de9cdccf4af6b318d8901e762e2deb0c0bd6
Oleksii Matviichuk <alexmatvichuk@gmail.com>
2021-09-05 16:03:35 +0200

    added btn

--- src/components/Contact/ContactList.js
+++ src/components/Contact/ContactList.js
@@ -1,9 +1,12 @@
+import { Button } from "./ContactList.styled";
+
 function ContactList({ data }) {
   return (
     <>
       {data.map(({ name, id, number }) => (
         <li key={id}>
           {name} : {number}
+          <Button>Delete</Button>
         </li>
       ))}
     </>
